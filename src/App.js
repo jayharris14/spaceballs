f810336ac6cc366c66a895ff4f31091a922a898a
jayharris14 <70274419+jayharris14@users.noreply.github.com>
2025-02-26 03:20:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6727,7 +6727,7 @@ function GetScore(){
     }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
-       "Access-Control-Allow-Origin": '*'
+       'Access-Control-Allow-Origin': '*'
     },
   })
      .then((response) => response.json())
@@ -6749,7 +6749,7 @@ function App() {
   var scores=[];
   useEffect(() => {
     fetch('https://spaceballs.us-east-2.elasticbeanstalk.com/scores', 
-      "Access-Control-Allow-Origin": '*'
+      'Access-Control-Allow-Origi': '*'
     })
     .then((res) => res.json())
     .then((data)=>setTopscores(data));
